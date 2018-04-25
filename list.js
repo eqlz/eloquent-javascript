a9072521cf7dbe24de553603dfed913589a1ace6
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-04-24 22:18:26 -0400

    Add comments to explain structure of a list and functions purpose

--- list.js
+++ list.js
@@ -1,3 +1,20 @@
+/*
+
+What is a list? A list takes the following structure:
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+*/
+
+// Transform an array to a list
 function arrayToList(array) {
     var list = null;
     for (var i = array.length - 1; i >= 0; i--) {
@@ -6,6 +23,7 @@ function arrayToList(array) {
     return list;
 }
 
+// Transform a list to an array
 function listToArray(list) {
     var array = [];
     for (var node = list; node; node = node.rest) {
@@ -13,3 +31,8 @@ function listToArray(list) {
     }
     return array;
 }
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
